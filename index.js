408bc843c28e56aa2ce39774f48df8d39854e6f4
Pratyush Mishra <pratyushdinesh56@gmail.com>
2025-04-15 22:29:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
   password: "password",
-  port: 3000,
+  port: 5432,
 });
 db.connect();
 
